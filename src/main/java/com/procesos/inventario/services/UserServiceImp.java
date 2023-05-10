245f02f1c228dc9250ec240402cddc9b9d8ebb2e
Jerson Garcia <jersonjavier@gmail.com>
2023-05-09 20:31:36 -0500

    implementacion de bcrypt para el password

--- src/main/java/com/procesos/inventario/services/UserServiceImp.java
+++ src/main/java/com/procesos/inventario/services/UserServiceImp.java
@@ -4,6 +4,7 @@ import com.procesos.inventario.models.User;
 import com.procesos.inventario.repository.UserRepository;
 import com.procesos.inventario.utils.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,6 +16,8 @@ public class UserServiceImp implements UserService{
     private UserRepository userRepository;
     @Autowired
     private JWTUtil jwtUtil;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public User getUser(Long id){
         return userRepository.findById(id).get();
@@ -23,6 +26,7 @@ public class UserServiceImp implements UserService{
     @Override
     public Boolean createUser(User user) {
         try {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
             userRepository.save(user);
             return true;
         }catch (Exception e){
@@ -45,6 +49,7 @@ public class UserServiceImp implements UserService{
             userBD.setLastName(user.getLastName());
             userBD.setBirthday(user.getBirthday());
             userBD.setAddress(user.getAddress());
+            userBD.setPassword(passwordEncoder.encode(user.getPassword()));
             User userUp = userRepository.save(userBD);
             return true;
         }catch (Exception e){
@@ -59,7 +64,7 @@ public class UserServiceImp implements UserService{
             throw new RuntimeException("Usuario no encontrado!");
         }
 
-        if(!userBd.get().getPassword().equals(user.getPassword())){
+        if(!passwordEncoder.matches(user.getPassword(), userBd.get().getPassword())){
             throw new RuntimeException("La contraseña es incorrecta!");
         }
         return jwtUtil.create(String.valueOf(userBd.get().getId()),
